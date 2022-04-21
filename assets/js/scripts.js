ce77d6d1cb8f02fcc1600873116e9e9a1b8e457d
Oliver C <ocronk10@gmail.com>
2022-04-20 19:07:49 -0500

    Momment, date time format, and json response

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,7 +1,10 @@
-
+var city = "Denver";
+var key = "64f2ee2a8261daa4d9f780f5b365f275";
+var dateTime = moment().format("YYYY-MM-DD HH:MM:SS");
+console.log(dateTime);
 
 var cityHist = [];
-//Saves search history of city
+//places text value in input form into an array
 $(".search").on("click", function () {
   var city = $(this).parent(".btnPar").siblings(".textVal").val();
   cityHist.push(city);
@@ -9,7 +12,7 @@ $(".search").on("click", function () {
   getHistory();
 });
 
-//creates extra buttons based on history
+//extra buttons will be created in search history
 var contHistEl = $(".cityHist");
 function getHistory() {
   for (let i = 0; i < cityHist.length; i++) {
@@ -26,8 +29,33 @@ function getHistory() {
     rowEl.append(btnEl);
   }
 }
-
+//deletes duplicates
 function emptyHist() {
   contHistEl.empty();
 }
-// deletes duplicates
+
+// var getUrlCurrent = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}`;
+// var getUrlCurrentUV = `https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}`;
+// var getUrlFiveDay = `https://api.openweathermap.org/data/2.5/forecast?q=${city},us&mode=xml&appid=${key}`;
+
+// var getUrlIcons = `https://http://openweathermap.org/img`;
+
+// $.ajax({
+//   url: getUrlCurrent,
+// 	// dataType: "json",
+//   method: 'GET',
+// }).then(function (response) {
+//   console.log('AJAX Response');
+//   console.log(response);
+// });
+
+// fetch(getUrlCurrent)
+//   .then(function (response) {
+//     return response.json();
+//   })
+//   .then(function (data) {
+//     console.log('Fetch Response');
+//     console.log(data);
+//   });
+
+//imgEl.setAttribute('src',`http://openweathermap.org/img/w/${data.weather[0].icon}.png`);
