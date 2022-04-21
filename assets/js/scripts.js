e4cf79c9abdd04f1e373f00f7ff517196056eb2c
Oliver C <ocronk10@gmail.com>
2022-04-20 19:03:24 -0500

    Fixed seearch array which now properly stores and deletes data

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,5 +1,33 @@
 
+
+var cityHist = [];
+//Saves search history of city
 $(".search").on("click", function () {
   var city = $(this).parent(".btnPar").siblings(".textVal").val();
-  localStorage.setItem("city", city);
+  cityHist.push(city);
+  emptyHist();
+  getHistory();
 });
+
+//creates extra buttons based on history
+var contHistEl = $(".cityHist");
+function getHistory() {
+  for (let i = 0; i < cityHist.length; i++) {
+    var rowEl = $("<row>");
+    var btnEl = $("<button>");
+
+    rowEl.addClass("row histBtnRow");
+    btnEl.addClass("btn btn-outline-secondary");
+    btnEl.attr("type", "button");
+
+    btnEl.text(`${cityHist[i]}`);
+
+    contHistEl.append(rowEl);
+    rowEl.append(btnEl);
+  }
+}
+
+function emptyHist() {
+  contHistEl.empty();
+}
+// deletes duplicates
